13a51a8283646436b9a72b1104524ba0a86c3406
Alice <2272674323@qq.com>
2018-06-06 22:52:39 +0800

    python基本操作

---
+++ 01_HelloWorld/01_demo.py
@@ -0,0 +1,34 @@
+# 输出处理, 多参数
+print(100+200)
+
+print('hello world')
+
+print('233', 'hello', 'life')
+
+print('100 + 400 =', 100+400)
+
+# 输入处理
+# name = input()
+# print('your name is: ', name)
+
+# python使用缩进进行编程
+a = 100
+if a >= 0:
+    print('a大于等于100')
+else:
+    print('a不是大于等于100')
+
+# python大小写敏感    
+
+# 转义
+print('I\'m is \"ok\"')
+
+# ''' '''表示多行
+print('''
+line1
+line2
+line3
+''')
+
+# 布尔类型
+print(3 > 2)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # GetPython
 学了一下叫Python的东西~
+
+
+# Python 小技巧
+1. 进入Python环境, 考虑(如Mac自带Python2.x版本)多个版本, 装了Python3.x的版本, 终端运行 python3 即可
+
+2. 退出 Python 终端环境: ctrl+d; 或者 运行 exit();
+
+3. 终端 python 终端清屏: ctrl+l  
+
+
+
